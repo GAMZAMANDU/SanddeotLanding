d4750c9331822889d56a20f86472ab2529187705
gamzamandu <galbimandu1126@gmail.com>
2024-07-10 14:11:08 +0900

    ADD : section

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const findGrid = document.getElementById("grid"); // "grid"로 수정
+
+    if (findGrid) {
+        for (let i = 0; i < 416; i++) {
+            findGrid.innerHTML += `<div class="gird-box"></div>`;
+        }
+    } else {
+        console.error('타겟 엘리먼트가 존재하지 않습니다.');
+    }
+});

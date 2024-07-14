d3c66ca66b19ab50fae4b6c19a39ddb2201d5262
gamzamandu <galbimandu1126@gmail.com>
2024-07-14 15:30:47 +0900

    특수문법 예시 완성 및 푸터 추가

--- analysis/aiEx2.js
+++ analysis/aiEx2.js
@@ -1,17 +1,16 @@
-let has = false;
+let has1 = false;
 
 window.addEventListener('scroll', function() {
-    if (has) return;
+    if (has1) return;
 
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 260) {
-        console.log('스크롤이 일정 이상 내려갔습니다.');
-        has = true;
+    if (scrollPosition > 300) {
+        has1 = true;
 
-        let target = document.getElementById('ai-exp');
-        let aiResElement = document.querySelector('.AI-res');
-        let text = "새로운 소프트웨어 업데이트가 시스템 성능을 크게 향상시킬 것입니다";
+        let target = document.getElementById('ai-exp2');
+        let aiResElement = document.querySelector('.AI-res2');
+        let text = `새로운 소프트웨어 ...는 시스템의 *[행동]을 크게 향상시킬 것이다. ?[performance가 어떻게 해석해야 자연스러워?]`;
 
         let index = 0;
         const intervalId = setInterval(() => {
@@ -24,8 +23,8 @@ window.addEventListener('scroll', function() {
                     setTimeout(() => {
                         aiResElement.style.display = 'block';
                         aiResElement.style.transform = 'scale(1)';
-                    }, 50); 
+                    }, 70); 
             }
-        }, 50 - (index * 2.5)); 
+        }, 50 - (index * 3)); 
     }
 });

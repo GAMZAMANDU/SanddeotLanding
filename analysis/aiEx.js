1317a529494a44a1359a0322206ce8e567fba7c6
gamzamandu <galbimandu1126@gmail.com>
2024-07-14 15:15:56 +0900

    AI평가 예시 완성

---
+++ analysis/aiEx.js
@@ -0,0 +1,35 @@
+let has = false;
+
+window.addEventListener('scroll', function() {
+    if (has) return;
+
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 260) {
+        console.log('스크롤이 일정 이상 내려갔습니다.');
+        has = true;
+
+        const target = document.getElementById('ai-exp');
+        const aiResElement = document.querySelector('.AI-res');
+        const text = "새로운 소프트웨어 업데이트가 시스템 성능을 크게 향상시킬 것입니다";
+
+        let index = 0;
+        const intervalId = setInterval(() => {
+            target.value += text[index];
+            index++;
+    
+            if (index >= text.length) {
+                clearInterval(intervalId);
+
+                    setTimeout(() => {
+                        aiResElement.style.display = 'block';
+                        aiResElement.style.transform = 'scale(1)';
+                    }, 50); 
+            }
+        }, 50 - (index * 2.5)); 
+
+
+    }
+
+
+});
